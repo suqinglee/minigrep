a1f1e29f9c9c803bd0e2f1e4710ab78cd41234f7
suqinglee <suqinglee@outlook.com>
2021-06-20 19:44:46 +0800

    grep key filename

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::error::Error;
+use std::fs;
+use std::env;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in if config.sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    } {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub sensitive: bool,
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let (query, filename) = (args[1].clone(), args[2].clone());
+        let sensitive = env::var("INSENSITIVE").is_err();
+        Ok(Config { sensitive, query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
